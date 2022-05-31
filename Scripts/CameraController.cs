eca6690a1606c1a4e2c5942ee86b4c49613689fb
Abel Horneland <abel.horneland@gmail.com>
2022-05-31 20:05:05 +0200

    Inital commit

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Cam;
+    
+    [SerializeField, Range(0f, 100f)]
+    float speed = 12f;
+    [SerializeField, Range(0f, 500f)]
+    float mouseSensitivity = 100f;
+
+    float yRotation = 0f;
+    float xRotation = 0f;
+    float mouseX;
+    float mouseY;
+    Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+        movement.y = Input.GetKey("space") ? 1 : Input.GetKey("c") ? -1: 0;
+
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        yRotation -= mouseY;
+        xRotation += mouseX;
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+        Cam.localRotation = Quaternion.Euler(yRotation, xRotation, 0f);
+
+        Cam.Translate(movement * speed * Time.deltaTime);
+    }
+}
